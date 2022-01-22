367c36f5bcebf02108100c3bac86e95d4f4c44eb
Gavish Poddar <gavishpoddar@hotmail.com>
2022-01-23 01:14:25 +0530

    Update tree.py

--- usp/tree.py
+++ usp/tree.py
@@ -30,7 +30,7 @@ _UNPUBLISHED_SITEMAP_PATHS = {
 """Paths which are not exposed in robots.txt but might still contain a sitemap."""
 
 
-def sitemap_tree_for_homepage(homepage_url: str, web_client: Optional[AbstractWebClient] = None) -> AbstractSitemap:
+def sitemap_tree_for_homepage(homepage_url, recursion_level=1, web_client=None):
     """
     Using a homepage URL, fetch the tree of sitemaps and pages listed in them.
 
@@ -53,7 +53,7 @@ def sitemap_tree_for_homepage(homepage_url: str, web_client: Optional[AbstractWe
 
     sitemaps = []
 
-    robots_txt_fetcher = SitemapFetcher(url=robots_txt_url, web_client=web_client, recursion_level=0)
+    robots_txt_fetcher = SitemapFetcher(url=robots_txt_url, web_client=web_client, recursion_level=recursion_level)
     robots_txt_sitemap = robots_txt_fetcher.sitemap()
     sitemaps.append(robots_txt_sitemap)
 
@@ -71,7 +71,7 @@ def sitemap_tree_for_homepage(homepage_url: str, web_client: Optional[AbstractWe
             unpublished_sitemap_fetcher = SitemapFetcher(
                 url=unpublished_sitemap_url,
                 web_client=web_client,
-                recursion_level=0,
+                recursion_level=recursion_level,
             )
             unpublished_sitemap = unpublished_sitemap_fetcher.sitemap()
 
